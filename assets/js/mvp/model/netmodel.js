2192b1eca998f75aeaf62ca40e8c6504d69b035e
James Stanton <jestanton@outlook.com>
2019-12-19 19:36:06 +0000

    added auth req func and event

--- assets/js/mvp/model/netmodel.js
+++ assets/js/mvp/model/netmodel.js
@@ -4,17 +4,20 @@ class NetModel extends BaseModel {
     constructor () {
         super('net')
         this.init()
-        
     }
 
     init() {
         super.init()
 
-
+        this.onAuthRequestResponse = new B2Event('authrequestresponse')
     }
 
     destroy() {
         super.destroy()
+    }
+
+    sendAuthRequest(username, password) {
+        console.error("sendAuthRequest() not implemented")
     }
 }
 
