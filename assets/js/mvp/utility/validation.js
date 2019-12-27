aaa14e712b7bbf8ef9ebba78e72fb8eb2056018e
James Stanton <jestanton@outlook.com>
2019-12-27 17:14:30 +0000

    added missing eol check for password chars

--- assets/js/mvp/utility/validation.js
+++ assets/js/mvp/utility/validation.js
@@ -1,7 +1,7 @@
 const noSpaceAtStart = /^[^\s]+/
 const usernameValidChars = /^[ー一-龯ぁ-ゞァ-ヶ -~Ａ-ｚ０-９！-／：-＠［-｀｛-～、-〜“”‘’´・ 　]+$/
 const emailValidFormat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-const passwordValidChars = /^[ -~]+/
+const passwordValidChars = /^[ -~]+$/
 const numberAny = /[0-9]/
 const lowercaseAny = /[a-z]/
 
