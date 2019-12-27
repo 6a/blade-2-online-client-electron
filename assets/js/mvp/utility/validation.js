ad5a3ba2a241e1d54ca435b8e75aa1d0d8221aeb
James Stanton <jestanton@outlook.com>
2019-12-27 16:19:59 +0000

    tweaked username regex

--- assets/js/mvp/utility/validation.js
+++ assets/js/mvp/utility/validation.js
@@ -1,5 +1,5 @@
 const noSpaceAtStart = /^[^\s]+/
-const usernameValidChars = /^[一-龯ぁ-ゞァ-ヶA-zＡ-ｚ０-９0-9「」［］【】（）＜＞《》≪≫ー～々〆〤！？～!?<>・\-_… 　]+$/
+const usernameValidChars = /^[ー一-龯ぁ-ゞァ-ヶ -~Ａ-ｚ０-９！-／：-＠［-｀｛-～、-〜“”‘’´・ 　]+$/
 const emailValidFormat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 const passwordValidChars = /^[ -~]+/
 const numberAny = /[0-9]/
