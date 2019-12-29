8aae21db5110a9825e68e711e4be462d55f4649c
James Stanton <jestanton@outlook.com>
2019-12-29 04:22:02 +0000

    changed email verification

--- assets/js/mvp/utility/validation.js
+++ assets/js/mvp/utility/validation.js
@@ -1,6 +1,6 @@
 const noSpaceAtStart = /^[^\s]+/
 const usernameValidChars = /^[ー一-龯ぁ-ゞァ-ヶ -~Ａ-ｚ０-９！-／：-＠［-｀｛-～、-〜“”‘’´・ 　]+$/
-const emailValidFormat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const emailValidFormat = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
 const passwordValidChars = /^[ -~]+$/
 const numberAny = /[0-9]/
 const lowercaseAny = /[a-z]/
