9132271b65f6c3b78b1e699cb55bc091ad9fe98e
James Stanton <jestanton@outlook.com>
2019-12-30 00:53:34 +0000

    Added app config helper

---
+++ assets/js/appconfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "blade-2-client",
+    version: "0.0.1",
+    api: "https://herwrbjh6a.execute-api.eu-west-2.amazonaws.com/v1",
+    users: "users",
+    leaderboards: "leaderboards",
+    match: "matches",
+    auth: "auth"
+}
