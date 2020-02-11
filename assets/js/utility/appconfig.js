21abdefe64074adcfffbe6ef448487067c96158d
James Stanton <jestanton@outlook.com>
2020-02-11 03:12:51 +0000

    new api endpoint url

--- assets/js/utility/appconfig.js
+++ assets/js/utility/appconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "blade-2-client",
     version: "0.0.1",
-    apiURL: "https://herwrbjh6a.execute-api.eu-west-2.amazonaws.com/v1",
+    apiURL: "https://rahvicrr4g.execute-api.eu-west-2.amazonaws.com/v1",
     usersPath: "users",
     authPath: "auth",
     leaderboardsPath: "leaderboards",
