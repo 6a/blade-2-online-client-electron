4ab2f20ce5b97b0863f8ee84fee7fe168c0846d2
James Stanton <jestanton@outlook.com>
2020-04-28 19:32:19 +0100

    updated username valid regex

--- assets/js/mvp/utility/validation.js
+++ assets/js/mvp/utility/validation.js
@@ -1,5 +1,5 @@
 const noSpaceAtStart = /^[^\s]+/
-const usernameValidChars = /^[ー一-龯ぁ-ゞァ-ヶ -~Ａ-ｚ０-９！-／：-＠［-｀｛-～、-〜“”‘’´・ 　]+$/
+const usernameValidChars = /^[ー一-龯ぁ-ゞァ-ヶ --/-9Ａ-ｚ０-９！-／;-~：-＠［-｀｛-～、-〜“”‘’´・　]+$/
 const emailValidFormat = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
 const passwordValidChars = /^[ -~]+$/
 const numberAny = /[0-9]/
