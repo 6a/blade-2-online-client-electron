423b9252721ada6b76b932698a6fd6b187dbe9d4
James Stanton <jestanton@outlook.com>
2020-05-15 23:55:53 +0100

    Avatar switching implemented

--- assets/js/mvp/view/profileview.js
+++ assets/js/mvp/view/profileview.js
@@ -3,6 +3,23 @@ const ProfilePresenter = require('../presenter/profilepresenter.js')
 const MatchHistoryRow = require('../utility/containers').MatchHistoryRow
 const Localization = require('../../utility/localization')
 
+const AVATAR_IMAGES = [
+    'portrait-laura',
+    'portrait-alisa',
+    'portrait-elliot',
+    'portrait-emma',
+    'portrait-fie',
+    'portrait-gaius',
+    'portrait-jusis',
+    'portrait-machius',
+    'portrait-millium',
+    'portrait-rean',
+]
+
+class AvatarChangeDirection{}
+AvatarChangeDirection.LEFT = -1
+AvatarChangeDirection.RIGHT = 1
+
 class ProfileView extends BaseView {
     constructor (viewsList) {
         super('profile', ProfilePresenter, viewsList, 'hidden')
@@ -15,6 +32,10 @@ class ProfileView extends BaseView {
         this.getElementReferences()
         this.addEventListeners()
         this.addTabbables()
+
+        this._avatarIndex = 0
+
+        this.resetUserDetails()
     }
 
     destroy() {
@@ -29,19 +50,37 @@ class ProfileView extends BaseView {
         this._loadingErrorControls = document.getElementById('profile-error-controls')
         this._matchHistoryContainer = document.getElementById('profile-match-history-list-wrapper')
 
+        this._avatar = document.getElementById('profile-avatar-image')
+        this._elo = document.getElementById('profile-elo-rating')
+        this._stats = {
+            victories: document.getElementById('profile-match-stats-victories'),
+            draws: document.getElementById('profile-match-stats-draws'),
+            defeats: document.getElementById('profile-match-stats-defeats'),
+            total: document.getElementById('profile-match-stats-total'),
+            winratio: document.getElementById('profile-match-stats-winratio')
+        }
+
         this._returnButton = document.getElementById('profile-return-button')
         this._retryButton = document.getElementById('profile-retry-button')
+        this._avatarButtonLeft = document.getElementById('profile-avatar-button-left')
+        this._avatarButtonRight = document.getElementById('profile-avatar-button-right')
     }
 
     addEventListeners() {
         this._returnButton.addEventListener('click', this.onReturnClicked.bind(this), false)
-        this._retryButton.addEventListener('click', this.initialiseProfileFetch.bind(this), false)
+        this._retryButton.addEventListener('click', this.onRetryButtonClicked.bind(this), false)
+        this._avatarButtonLeft.addEventListener('click', this.onAvatarSelectLeft.bind(this), false)
+        this._avatarButtonRight.addEventListener('click', this.onAvatarSelectRight.bind(this), false)
 
         document.addEventListener('keydown', this.onEscDown.bind(this), false)
     }
 
     removeEventListeners() {
         this._returnButton.removeEventListener('click', this.onReturnClicked.bind(this), false)
+        this._retryButton.removeEventListener('click', this.onRetryButtonClicked.bind(this), false)
+        this._avatarButtonLeft.removeEventListener('click', this.onAvatarSelectLeft.bind(this), false)
+        this._avatarButtonRight.removeEventListener('click', this.onAvatarSelectRight.bind(this), false)
+
 
         document.removeEventListener('keydown', this.onEscDown.bind(this), false)
     }
@@ -59,16 +98,30 @@ class ProfileView extends BaseView {
         this._presenter.closeForm()
     }
 
-    initialiseProfileFetch(event) {
+    onRetryButtonClicked(event) {
+        event.preventDefault()
         this._loadingOverlay.classList.remove('hidden')
         this._loadingSpinner.classList.remove('hidden')
         this._loadingErrorControls.classList.add('hidden')
         this._presenter.requestMatchHistory()
     }
 
+    onAvatarSelectLeft(event) {
+        event.preventDefault()
+
+        this.changeAvatarImage(AvatarChangeDirection.LEFT)
+    }
+
+    
+    onAvatarSelectRight(event) {
+        event.preventDefault()
+
+        this.changeAvatarImage(AvatarChangeDirection.RIGHT)
+    }
+
     setActive(active) {
         if (active) {
-            this.initialiseProfileFetch()
+            this.onRetryButtonClicked()
         }
 
         super.setActive(active)
@@ -105,6 +158,39 @@ class ProfileView extends BaseView {
         }
 
         this._matchHistoryContainer.innerHTML = matchHistoryString
+    }
+
+    resetUserDetails() {
+        this._currentMatchHistory = null
+        this._currentPublicID = null
+        this._currentProfile = null
+
+        this._elo.innerHTML = ''
+
+        for (let index = 0; index < this._stats.length; index++) {
+            this._stats[index].innerHTML = ''
+            
+        }
+    }
+
+    /**
+     * Change the current avatar image, as well as the internal index, in the specified direction
+     * @param {AvatarChangeDirection} direction The direction to iterate over the avatar collection
+     */
+    changeAvatarImage(direction) {
+        if (direction === AvatarChangeDirection.LEFT || direction === AvatarChangeDirection.RIGHT) {
+            this._avatarIndex = this._avatarIndex + direction
+
+            if (this._avatarIndex < 0) {
+                this._avatarIndex = AVATAR_IMAGES.length -1
+            } else if (this._avatarIndex >= AVATAR_IMAGES.length) {
+                this._avatarIndex = 0
+            }
+
+            let newAvatarString = AVATAR_IMAGES[this._avatarIndex]
+
+            this._avatar.src = `../assets/images/character-portraits/${newAvatarString}.png`
+        } 
     }
 }
 
