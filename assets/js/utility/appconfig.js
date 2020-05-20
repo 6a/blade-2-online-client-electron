0e25ebf4b3c72b69ee415f12f14ec100b6be84e6
James Stanton <jestanton@outlook.com>
2020-05-20 23:17:37 +0100

    Updated version

--- assets/js/utility/appconfig.js
+++ assets/js/utility/appconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "blade-2-client",
-    version: "0.3.0",
+    version: "0.4.0",
     apiURL: "https://rahvicrr4g.execute-api.eu-west-2.amazonaws.com/v1",
     usersPath: "users",
     authPath: "auth",
